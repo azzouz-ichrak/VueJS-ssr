881df75a53184fd1f57628914a9785044b7ddb01
Azzouz-ichrak <ichrak12.azzouz@gmail.com>
2021-08-19 13:43:43 +0100

    ssr add

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="content">
+     -- {{ count }} --<br />
+    <button v-on:click.prevent="increment">+</button>
+    <button v-on:click.prevent="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: () => {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      if (this.count > 0) {
+        this.count--;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  padding: 5%;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createRouter } from './router/router.js'
 
-Vue.config.productionTip = false
+// export a factory function for creating fresh app, router and store
+// instances
+export function createApp() {
+    // create router instance
+    const router = createRouter();
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    const app = new Vue({
+        router,
+        // the root instance simply renders the App component.
+        render: h => h(App)
+    });
+
+    return { app, router };
+}
